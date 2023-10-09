4cabaad546d5495ac3677f02655915ef82eefd93
anshugpt <138896567+anshugpt@users.noreply.github.com>
2023-10-09 10:11:15 +0530

    docs

---
+++ app.js
@@ -0,0 +1,16 @@
+// * For loop => it repete the piece of code.
+
+//  for (initilize first using let; now give condition ; updation){
+    // * Piece of code                                                                 // syntax of for loop
+// } 
+
+// print 1 2 3 ...5 using for loop
+
+for(let i = 1 ; i <= 5; i++){
+    // console.log(i);
+}
+
+// console.log(i); => 1, 2, 3, 4, 5 
+// console.log("Anshu"); => Anshu, Anshu, Anshu, Anshu, Anshu
+// let i = "Anshu" => nothing say on console window
+
